e042111b2a42d3120575ab663184b6001ffa23cd
Isreal6ix <isrealsix@gmail.com>
2021-08-12 19:39:32 +0700

    completed the widget on the Edit user

--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -4,6 +4,7 @@ import {
 	MailOutline,
 	PermIdentity,
 	PhoneAndroid,
+	Publish,
 } from '@material-ui/icons';
 import classes from './User.module.css';
 
@@ -60,7 +61,74 @@ const User = () => {
 						</div>
 					</div>
 				</div>
-				<div className={classes.userUpdate}></div>
+				<div className={classes.userUpdate}>
+					<span className={classes.userUpdateTitle}>Edit</span>
+
+					<form className={classes.userUpdateForm}>
+						<div className={classes.userUpdateLeft}>
+							<div className={classes.userUpdateItem}>
+								<label>Username</label>
+								<input
+									type="text"
+									placeholder="anna4daves"
+									className={classes.userUpdateInput}
+								/>
+							</div>
+
+							<div className={classes.userUpdateItem}>
+								<label>Full Name</label>
+								<input
+									type="text"
+									placeholder="Anna Daves"
+									className={classes.userUpdateInput}
+								/>
+							</div>
+
+							<div className={classes.userUpdateItem}>
+								<label>Date of Birth</label>
+								<input
+									type="text"
+									placeholder="20.12.2000"
+									className={classes.userUpdateInput}
+								/>
+							</div>
+
+							<div className={classes.userUpdateItem}>
+								<label>Phone</label>
+								<input
+									type="text"
+									placeholder="8(998)-456-12-65"
+									className={classes.userUpdateInput}
+								/>
+							</div>
+
+							<div className={classes.userUpdateItem}>
+								<label>Email</label>
+								<input
+									type="text"
+									placeholder="anna4daves@mail.in"
+									className={classes.userUpdateInput}
+								/>
+							</div>
+						</div>
+
+						<div className={classes.userUpdateRight}>
+							<div className={classes.userUpdateUpload}>
+								<img
+									src="https://images.pexels.com/photos/4240503/pexels-photo-4240503.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+									alt=""
+									className={classes.userUpdateImg}
+								/>
+
+								<label htmlFor="file">
+									<Publish className={classes.userUpdateIcon} />
+								</label>
+								<input type="file" id="file" style={{ display: 'none' }} />
+							</div>
+							<button className={classes.userUpdateButton}>Update</button>
+						</div>
+					</form>
+				</div>
 			</div>
 		</div>
 	);
