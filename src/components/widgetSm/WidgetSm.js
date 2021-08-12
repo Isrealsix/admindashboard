07479567d32e17c3ed19cc3d967bcf3181bd46c8
Isreal6ix <isrealsix@gmail.com>
2021-08-12 13:30:27 +0700

    completed the small widget styling

--- src/components/widgetSm/WidgetSm.js
+++ src/components/widgetSm/WidgetSm.js
@@ -1,6 +1,92 @@
+import { Visibility } from '@material-ui/icons';
 import classes from './WidgetSm.module.css';
 const WidgetSm = () => {
-	return <div className={classes.widgetSm}>small</div>;
+	return (
+		<div className={classes.widgetSm}>
+			<span className={classes.widgetSmTitle}>New Members</span>
+			<ul className={classes.widgetSmList}>
+				<li className={classes.widgetSmListItem}>
+					<img
+						src="https://images.pexels.com/photos/2467389/pexels-photo-2467389.jpeg"
+						alt="profile_pic"
+						className={classes.widgetSmImg}
+					/>
+					<div className={classes.widgetSmUser}>
+						<span className={classes.widgetSmUsername}>Alina Gretchen</span>
+						<span className={classes.widgetSmUserTitle}>Programmer</span>
+					</div>
+					<button className={classes.widgetSmButton}>
+						<Visibility className={classes.widgetSmIcon} />
+						Display
+					</button>
+				</li>
+
+				<li className={classes.widgetSmListItem}>
+					<img
+						src="https://images.pexels.com/photos/2467389/pexels-photo-2467389.jpeg"
+						alt="profile_pic"
+						className={classes.widgetSmImg}
+					/>
+					<div className={classes.widgetSmUser}>
+						<span className={classes.widgetSmUsername}>Alina Gretchen</span>
+						<span className={classes.widgetSmUserTitle}>Programmer</span>
+					</div>
+					<button className={classes.widgetSmButton}>
+						<Visibility className={classes.widgetSmIcon} />
+						Display
+					</button>
+				</li>
+
+				<li className={classes.widgetSmListItem}>
+					<img
+						src="https://images.pexels.com/photos/2467389/pexels-photo-2467389.jpeg"
+						alt="profile_pic"
+						className={classes.widgetSmImg}
+					/>
+					<div className={classes.widgetSmUser}>
+						<span className={classes.widgetSmUsername}>Alina Gretchen</span>
+						<span className={classes.widgetSmUserTitle}>Programmer</span>
+					</div>
+					<button className={classes.widgetSmButton}>
+						<Visibility className={classes.widgetSmIcon} />
+						Display
+					</button>
+				</li>
+
+				<li className={classes.widgetSmListItem}>
+					<img
+						src="https://images.pexels.com/photos/2467389/pexels-photo-2467389.jpeg"
+						alt="profile_pic"
+						className={classes.widgetSmImg}
+					/>
+					<div className={classes.widgetSmUser}>
+						<span className={classes.widgetSmUsername}>Alina Gretchen</span>
+						<span className={classes.widgetSmUserTitle}>Programmer</span>
+					</div>
+					<button className={classes.widgetSmButton}>
+						<Visibility className={classes.widgetSmIcon} />
+						Display
+					</button>
+				</li>
+
+				<li className={classes.widgetSmListItem}>
+					<img
+						src="https://images.pexels.com/photos/2467389/pexels-photo-2467389.jpeg"
+						alt="profile_pic"
+						className={classes.widgetSmImg}
+					/>
+					<div className={classes.widgetSmUser}>
+						<span className={classes.widgetSmUsername}>Alina Gretchen</span>
+						<span className={classes.widgetSmUserTitle}>Programmer</span>
+					</div>
+					<button className={classes.widgetSmButton}>
+						<Visibility className={classes.widgetSmIcon} />
+						Display
+					</button>
+				</li>
+			</ul>
+		</div>
+	);
 };
 
 export default WidgetSm;
